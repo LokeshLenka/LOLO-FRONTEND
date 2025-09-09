e5464a9dd98d66b3e1fbede7122d2c6efffe07b3
Lokesh <lenkalokesh98@gmail.com>
2025-09-09 17:47:47 +0530

    add:added signup form

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import "./App.css";
 import { Route, Router, Routes } from "react-router-dom";
 import Home from "./Pages/Home";
 import { AnimatePresence } from "motion/react";
+import MainLayout from "./layouts/MainLayout";
+import { Signup } from "./Pages/Signup";
 
 function App() {
   // const [count, setCount] = useState(0);
@@ -11,7 +13,11 @@ function App() {
     <AnimatePresence mode="wait">
       {/* <ScrollToTop /> */}
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<MainLayout />}>
+          <Route index element={<Home />} />
+          {/* <Route path="*" element={<div>404 Not Found</div>} /> */}
+          <Route path="/signup" element={<Signup />} />
+        </Route>
       </Routes>
     </AnimatePresence>
   );
---
+++ src/Pages/Signup.tsx
@@ -0,0 +1,10 @@
+import SignUpForm from "@/components/SignUpForm";
+import React, { Component } from "react";
+
+export class Signup extends Component {
+  render() {
+    return <SignUpForm />;
+  }
+}
+
+export default Signup;
