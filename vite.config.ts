fbd01cc11ec2af4ae398136842661e9081e0d695
Lokesh <lenkalokesh98@gmail.com>
2025-10-24 20:59:22 +0530

    chore: update npm packages and adjust Vite config for improved compatibility

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,11 @@ import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import react from "@vitejs/plugin-react-swc";
+import svgr from "vite-plugin-svgr";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths(), tailwindcss()],
+  plugins: [react(), tsconfigPaths(), tailwindcss(), svgr()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
