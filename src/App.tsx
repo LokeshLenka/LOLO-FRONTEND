5afa120505e9f027fca528119caf7a37a5233e7a
Lokesh <lenkalokesh98@gmail.com>
2025-10-24 21:47:51 +0530

    Replace references to old page structure, add authentication and routing improvements

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,38 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
-import { Route, Router, Routes } from "react-router-dom";
-import Home from "./Pages/Home";
+import { Route, Router, Routes, useLocation } from "react-router-dom";
 import { AnimatePresence } from "motion/react";
-import SignUp from "./Pages/SignUp";
+import SignUp from "./pages/SignUp";
 import MainLayout from "./layouts/MainLayout";
-import LoginPage from "./Pages/Login";
+import LoginPage from "./pages/Login";
+import { AuthProvider } from "./context/AuthContext";
+import NotFound from "./pages/OtherPages/NotFound";
+import Login from "./pages/Login";
+import { ScrollToTop } from "./components/common/ScrollToTop";
+import { Settings } from "lucide-react";
+import UserDashboard from "./pages/Dashboards/UserDashboard";
+import AppLayout from "./layouts/AppLayout";
+import Home from "./pages/Home";
 
 function App() {
-  // const [count, setCount] = useState(0);
-
   return (
-    <AnimatePresence mode="wait">
-      {/* <ScrollToTop /> */}
-      <Routes>
-        <Route path="/" element={<MainLayout />}>
-          <Route index element={<Home />} />
-        </Route>
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/login" element={<LoginPage />} />
-      </Routes>
-    </AnimatePresence>
+    <AuthProvider>
+      <AnimatePresence mode="wait">
+        <ScrollToTop />
+        <Routes>
+          <Route path="/" element={<MainLayout />}>
+            <Route index element={<Home />} />
+          </Route>
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/dashboard" element={<AppLayout />}>
+            {/* <Route index element={<Profile />} /> */}
+            <Route path="settings" element={<Settings />} />
+          </Route>
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </AnimatePresence>
+    </AuthProvider>
   );
 }
 
