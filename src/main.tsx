e6851425987be7d29bd2d9b13c12fa27163a0777
Lokesh <lenkalokesh98@gmail.com>
2025-10-24 21:47:51 +0530

    Update main application entry point and context providers

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,19 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import App from './App';
-import './index.css';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+import "./index.css";
+import { ThemeProvider } from "./context/ThemeContext";
+import { AppWrapper } from "./components/common/PageMeta";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <ThemeProvider>
+      <BrowserRouter>
+        <AppWrapper>
+          <App />
+        </AppWrapper>
+      </BrowserRouter>
+    </ThemeProvider>
   </StrictMode>
 );
