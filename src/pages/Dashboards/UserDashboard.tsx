f6b293fd3bbd3be2579640532e4b3a2ba7c786b9
Lokesh <lenkalokesh98@gmail.com>
2025-10-26 11:18:22 +0530

    refactor: clean up UserDashboard layout by removing commented-out components

--- src/pages/Dashboards/UserDashboard.tsx
+++ src/pages/Dashboards/UserDashboard.tsx
@@ -7,23 +7,6 @@ export default function UserDashboard() {
       <div className="grid grid-cols-12 gap-4 md:gap-6">
         <div className="col-span-12 space-y-6 xl:col-span-7">
           {/* <UserMetrics /> */}
-          <BasicTableOne/>
-
-          {/* <MonthlySalesChart /> */}
-        </div>
-
-        <div className="col-span-12 xl:col-span-5">
-          {/* <MonthlyTarget /> */}
-        </div>
-
-        <div className="col-span-12">{/* <StatisticsChart /> */}</div>
-
-        <div className="col-span-12 xl:col-span-5">
-          {/* <DemographicCard /> */}
-        </div>
-
-        <div className="col-span-12 xl:col-span-7">
-          {/* <RecentOrders /> */}
         </div>
       </div>
     </>
