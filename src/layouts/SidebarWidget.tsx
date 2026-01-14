d2f0752da85a53a813c88aa356fc8e5b084bf667
Lokesh <lenkalokesh98@gmail.com>
2026-01-14 10:33:32 +0530

    fix: update styles in SidebarWidget for improved consistency and accessibility

--- src/layouts/SidebarWidget.tsx
+++ src/layouts/SidebarWidget.tsx
@@ -16,18 +16,18 @@ export default function SidebarWidget() {
   const { logout } = useAuth();
 
   return (
-    <div className="fixed left-0 right-0 bottom-0 mx-auto w-full max-w-60 rounded-2xl bg-gray-50 px-4 py-5 text-center dark:bg-white/[0.03] z-50 mb-32 lg:mb-20">
+    <div className="fixed left-0 right-0 bottom-0 mx-auto w-full max-w-60 rounded-2xl bg-black/1 p-1 text-center dark:bg-white/1 z-50 mb-32 lg:mb-20">
       <AlertDialog>
         <AlertDialogTrigger asChild>
           <Button
             aria-label="Logout"
-            className="w-full h-12 flex items-center justify-center gap-2 py-4 px-6 rounded-lg font-semibold text-base bg-lolo-red text-white shadow-md hover:bg-lolo-red/80 focus:outline-none focus:ring-2 focus:ring-lolo-red/60 focus:ring-offset-2 transition-all duration-200"
+            className="w-full h-12 flex items-center justify-center gap-2 py-4 px-6 rounded-lg font-semibold text-base bg-lolo-red text-white shadow-md hover:bg-lolo-red/80  transition-all duration-200"
           >
             <span className="material-icons-outlined text-lg">Logout</span>
           </Button>
         </AlertDialogTrigger>
 
-        <AlertDialogContent>
+        <AlertDialogContent className="bg-white dark:bg-white/1 backdrop-blur-2xl border border-white/10 rounded-2xl shadow-lg">
           <AlertDialogHeader>
             <AlertDialogTitle>
               Are you sure you want to logout?
@@ -38,11 +38,11 @@ export default function SidebarWidget() {
           </AlertDialogHeader>
 
           <AlertDialogFooter>
-            <AlertDialogCancel className=" h-12 flex items-center justify-center gap-2 rounded-lg font-semibold text-base text-gray-700 bg-white/60 hover:bg-gray-100 shadow-sm focus:outline-none focus:ring-2 focus:ring-lolo-white/40 focus:ring-offset-2 transition-colors duration-200">
+            <AlertDialogCancel className="h-12 flex items-center justify-center gap-2 rounded-lg font-semibold text-base text-black dark:text-white  bg-white dark:bg-white/5 hover:dark:bg-gray-900 shadow-sm  transition-colors duration-200">
               Cancel
             </AlertDialogCancel>
             <AlertDialogAction
-              className="h-12 flex items-center justify-center gap-2 py-4 px-6 rounded-lg font-semibold text-base bg-lolo-red text-white shadow-md hover:bg-lolo-red/80 focus:outline-none focus:ring-2 focus:ring-lolo-red/60 focus:ring-offset-2 transition-all duration-200"
+              className="h-12 flex items-center justify-center gap-2 py-4 px-6 rounded-lg font-semibold text-base bg-lolo-red text-white shadow-md hover:bg-lolo-red/80  transition-all duration-200"
               onClick={logout}
             >
               Logout
