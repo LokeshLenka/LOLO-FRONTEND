b9e97a025f38584ee9cc6fb80810bbc782885179
Lokesh <lenkalokesh98@gmail.com>
2026-09-09 09:31:00 +0530

    fix(ui): update text color for Timeline item titles

--- src/components/Timeline.tsx
+++ src/components/Timeline.tsx
@@ -52,13 +52,13 @@ const TimelineItem = ({ item }: { item: TimelineEntry; index: number }) => {
           />
         </div>
 
-        <h3 className="hidden md:block text-xl md:pl-20 md:text-5xl font-bold text-neutral-500 ">
+        <h3 className="hidden md:block text-xl md:pl-20 md:text-5xl font-bold text-neutral-200 ">
           {item.title}
         </h3>
       </div>
 
       <div className="relative pl-20 pr-4 md:pl-4 w-full">
-        <h3 className="md:hidden block text-2xl mb-4 text-left font-bold text-neutral-500">
+        <h3 className="md:hidden block text-2xl mb-4 text-left font-bold text-neutral-200">
           {item.title}
         </h3>
         <div>{item.content}</div>
